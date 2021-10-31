84c487b41ff8b8e3ed5847f7048ea9089aba04cb
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-10-31 14:43:30 -0300

    Separando el perfil del usuario de los datos de la cuenta

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,24 +2,8 @@ class User < ApplicationRecord
     has_secure_password
 
     validates :email, :presence => true, uniqueness: { case_sensitive: false }
-    validates :document_number, :presence => true, uniqueness: true, numericality: { only_integer: true, greater_than_or_equal_to: 10000 }
     validates :password, length: { minimum: 8 }
-    validates :first_name, :presence => true, length: { minimum: 2 }
-    validates :last_name, :presence => true, length: { minimum: 2 }
-    validates :birthdate, :presence => true
-    validate :validate_birthdate?
-    validates :comorbidity, :inclusion => { :in => [true, false] }
     validates_presence_of :password, :on => [:create, :update]
-
-    def validate_birthdate?
-        return unless birthdate.present?
-        
-        if birthdate.before?(Date.new(1900, 1, 1))
-            errors.add(:birthdate, "must be after #{Date.new(1900, 1, 1)}")
-        elsif  birthdate.after?(Date.today - 6.years)
-            errors.add(:birthdate, "must be before #{Date.today - 6.years}")
-        end
-    end
 
     def User.digest(string)
         cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
