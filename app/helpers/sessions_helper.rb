4d90bc07b43d2159bd0228895c3c77497c4f3c2f
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-11-01 23:06:02 -0300

    Agregando tiempo de expiración a la sesión

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -10,4 +10,9 @@ module SessionsHelper
     def current_user
         @current_user ||= User.find(session[:user_id]) if !!session[:user_id]
     end
+
+    def session_expires
+        session[:expires_at] < Time.now
+        redirect_to auth_login_path
+    end
 end
