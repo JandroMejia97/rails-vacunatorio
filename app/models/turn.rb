de0aec4ef5ac78a7f6ed8a39f8a405687446f48d
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-11-03 14:13:56 -0300

    Agrengando validación

--- app/models/turn.rb
+++ app/models/turn.rb
@@ -3,12 +3,13 @@ class Turn < ApplicationRecord
     belongs_to :user
     belongs_to :campaign
     belongs_to :vaccination_center
-    belongs_to :vaccine
+    belongs_to :vaccine, optional: true
     validates :user_id, presence: true
     validates :campaign_id, presence: true
     validates :vaccination_center_id, presence: true
+    validates_presence_of :vaccine, :on => :update
     validates :status, inclusion: { in: Turn.statuses.keys }
-    validate :validate_date?
+    validate :validate_date?, :has_turn_in_campaign?
 
     def validate_date?
         return unless date?
@@ -25,5 +26,15 @@ class Turn < ApplicationRecord
             return false
         end
         return true
+    end
+
+    def has_turn_in_campaign?
+        return unless campaign_id
+        if Turn.where("campaign_id = ? AND (status = ? OR status = ?)", campaign_id, Turn.statuses[:pedding], Turn.statuses[:assigned]).exists?
+            errors.add(:campaign_id, I18n.t('validations.turn.has_turn_in_campaign'))
+            return false
+        else
+            return true
+        end
     end
 end
