04870ec04edc9358e21850e5ed2f59d6d853c54c
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-11-03 15:06:07 -0300

    Agregando traducciones

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,6 @@ Rails.application.routes.draw do
   # Sessions routes
   get '/auth/login', to: 'sessions#login'
   post '/auth/login', to: 'sessions#create'
-  post '/auth/logout', to: 'sessions#destroy'
   get '/auth/logout', to: 'sessions#destroy'
   # Profiles routes
   get '/profile/me', to: 'user_profiles#me', as: 'me'
