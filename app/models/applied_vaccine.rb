cadf1b74e517939107ee9f851dfca4e3674c8b49
Alejandro Mejia <alejandromejia2013.27@gmail.com>
2021-11-13 10:56:03 -0300

    Agregando lista de vacunas a la campaña

--- app/models/applied_vaccine.rb
+++ app/models/applied_vaccine.rb
@@ -3,5 +3,5 @@ class AppliedVaccine < ApplicationRecord
 
     validates :lot_number, presence: true, length: { maximum: 50, minimum: 3 }
     validates :applied_dose, presence: true, numericality: { only_integer: true, greater_than: 0 }
-    validates :vaccine_id, presence: true, blank: false
+    validates :vaccine_id, presence: true
 end
