247754f6e6b15ee0d19520d03bc1d06a4d246e63
Azul Angeles Martini Morelli <azul.martini@alu.ing.unlp.edu.ar>
2021-11-22 12:27:43 -0300

    HU modificar usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -19,7 +19,11 @@ Rails.application.routes.draw do
   get '/applied_vaccines/new', to: 'applied_vaccines#new', as: 'new_applied_vaccines'
   post '/applied_vaccines/new', to: 'turns#pending_turns'
 
+  
   # Profiles routes
+  get '/profile/modify', to: 'user_profiles#modify', as: 'modify'
+  post '/profile/modify', to: 'user_profiles#all_users'
+  patch '/profile/modify', to: 'user_profiles#update2', as:'update2'
   get '/profile/all_users', to: 'user_profiles#all_users', as: 'all_users'
   get '/profile/me', to: 'user_profiles#me', as: 'me'
   get '/profile/me/edit', to: 'user_profiles#edit', as: 'edit_profile'
