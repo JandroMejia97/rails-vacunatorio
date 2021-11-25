add72cfbc23a5f9c6d73a6a6a1a69de25311ed11
Azul Angeles Martini Morelli <azul.martini@alu.ing.unlp.edu.ar>
2021-11-25 10:12:35 -0300

    solucionando search

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_24_214917) do
+ActiveRecord::Schema.define(version: 2021_11_24_233325) do
 
   create_table "applied_vaccines", force: :cascade do |t|
     t.string "lot_number"
